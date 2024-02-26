c4ba0a1edcc05bc108b762d343079e0563690b18
Daniel Westergren <carl.dawe@gmail.com>
2024-02-26 04:45:44 +0100

    new module update

--- Retro-Cafe/src/modules/mainsection.jsx
+++ Retro-Cafe/src/modules/mainsection.jsx
@@ -1,14 +1,14 @@
 export function Mainsection({ main }) {
   return main.map((self) => {
     return (
-      <section id={self.name} style={{ backgroundImage: `url(src/assets/${self.name}.jpg)` }}>
+      <section id={self.name} key={self.name} style={{ backgroundImage: `url(src/assets/${self.name}.jpg)` }}>
         <h1>{self.name}</h1>
         <p>{self.text[0]}</p>
         <p>{self.text[1]}</p>
         <div className="list">
-          {self.list.map((s) => {
+          {self.list.map((s,i) => {
             return (
-              <div>
+              <div key={i}>
                 <b>{s.left}</b>
                 <b>{s.right}</b>
               </div>
